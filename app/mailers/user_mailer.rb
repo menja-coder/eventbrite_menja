3f20849fb6bea84b360ebeaa90a68589bf2c76ce
menja-coder <rakotomenja@gmail.com>
2019-08-16 00:43:05 +0300

    finalize backend

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,18 @@
 class UserMailer < ApplicationMailer
 
 	default from: 'no-reply@monsite.fr'
+
 	def welcome_email(user)
 		@user = user
 		mail(to: @user.email, subject: 'Bienvenue chez nous')
 	end
 
+	def email_for_admin(attendance)
+		@admin = attendance.event.admin
+		@participant = attendance.participant
+		@event = attendance.event
+		mail(to: @admin.email, subject: "Bonjour #{@admin.first_name}, #{@participant.first_name} a participé à votre évènement" )
+	end
 
 
 end
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,11 @@
 class Attendance < ApplicationRecord
 	belongs_to :event
 	belongs_to :participant, class_name: "User"
+
+	
+	after_create :sent_email_to_admin
+		def sent_email_to_admin
+			UserMailer.email_for_admin(self).deliver_now
+		end
+
 end
